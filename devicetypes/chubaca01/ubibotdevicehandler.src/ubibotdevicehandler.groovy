72d7f400eeabb7d3654a557be88c1ca0a21b8cd5
jean-pierre ruster <philippetourn@gmail.com>
2020-05-15 21:51:51 -0700

    update temperature colors

--- devicetypes/chubaca01/ubibotdevicehandler.src/ubibotdevicehandler.groovy
+++ devicetypes/chubaca01/ubibotdevicehandler.src/ubibotdevicehandler.groovy
@@ -44,13 +44,13 @@ metadata {
 			tileAttribute ("device.temperature", key: "PRIMARY_CONTROL") {
 				attributeState("temperature", label:'🏊🏼${currentValue}°', unit:"F",
                 backgroundColors:[
-                    [value: 31, color: "#153591"],
-                    [value: 44, color: "#1e9cbb"],
-                    [value: 59, color: "#90d2a7"],
-                    [value: 74, color: "#44b621"],
-                    [value: 84, color: "#f1d801"],
-                    [value: 95, color: "#d04e00"],
-                    [value: 96, color: "#bc2323"]
+                    [value: 12, color: "#cd68f8"],
+                    [value: 15, color: "#8268f8"],
+                    [value: 18, color: "#689ff8"],
+                    [value: 21, color: "#68d9f8"],
+                    [value: 24, color: "#68f876"],
+                    [value: 27, color: "#dbf868"],
+                    [value: 30, color: "#f8ab68"]
                 ]
             )
 			}
